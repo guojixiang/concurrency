4ecb898deb54819824977a752173b76ca0e40d69
guojixiang <945949480@qq.com>
2018-10-12 14:40:11 +0800

    inti project

---
+++ src/main/java/com/guojxiang/multithread/concurrency/ConcurrencyApplication.java
@@ -0,0 +1,12 @@
+package com.guojxiang.multithread.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyApplication.class, args);
+    }
+}
